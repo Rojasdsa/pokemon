cdd51174bc5fd331aff8b7f6c37777b14df1a69b
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-14 17:01:03 +0100

    añadido if a funcion subtype null

--- pokemon/public/js/custom.js
+++ pokemon/public/js/custom.js
@@ -1,14 +1,16 @@
-'use strict'
+'use strict';
 
 // Muestra la opción subtype=null cambiandole el color
 document.addEventListener('DOMContentLoaded', function () {
     let subtypeOption = document.querySelector('.hover-null');
 
-    subtypeOption.addEventListener('mouseover', function () {
-        subtypeOption.classList.add('hovered');
-    });
+    if (subtypeOption) {
+        subtypeOption.addEventListener('mouseover', function () {
+            subtypeOption.classList.add('hovered');
+        });
 
-    subtypeOption.addEventListener('mouseout', function () {
-        subtypeOption.classList.remove('hovered');
-    });
+        subtypeOption.addEventListener('mouseout', function () {
+            subtypeOption.classList.remove('hovered');
+        });
+    }
 });
