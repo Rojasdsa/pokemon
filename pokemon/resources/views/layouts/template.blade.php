66ef987b489611807c06f74cb2b12786e2f7b60a
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-16 21:54:08 +0100

    nueva referencia

--- pokemon/resources/views/layouts/template.blade.php
+++ pokemon/resources/views/layouts/template.blade.php
@@ -14,8 +14,8 @@
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
     {{-- css/js propios --}}
-    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
-    <script src="{{ asset('js/custom.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+    <script src="{{ asset('assets/js/custom.js') }}"></script>
 </head>
 
 <body>
