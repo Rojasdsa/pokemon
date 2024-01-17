8d9abd4fbfe699061f37299af42e4738c38a5883
Fernando Rojas <rojasdsa@alumnos.ilernasevilla.com>
2024-01-17 10:50:47 +0100

    Ajuste de rutas de estilo

--- pokemon/resources/views/layouts/template.blade.php
+++ pokemon/resources/views/layouts/template.blade.php
@@ -8,7 +8,12 @@
     <title>Pokémon</title>
 
     {{-- css/js de bootstrap --}}
-    @vite(['resources/js/app.js', 'resources/css/app.scss', 'resources/css/custom.css'])
+    @vite([
+        'resources/js/app.js', 
+        'resources/css/app.scss', 
+        'resources/css/app.css',
+        'resources/css/custom.scss'
+        ])
     {{-- Iconos --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
